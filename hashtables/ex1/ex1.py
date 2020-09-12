9d95c625981123f8e3941134e32afd70a2b8a713
patrick-replogle <patrickr1138@gmail.com>
2020-09-11 18:06:56 -0700

    ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,18 @@
 def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    hashMap = {}
+
+    for i in range(len(weights)):
+        curr = weights[i]
+        if (limit - curr) in hashMap:
+            return (i, hashMap[limit - curr])
+        else:
+            hashMap[curr] = i
 
     return None
+
+
+weights = [4, 6, 10, 15, 16]
+length = 5
+limit = 21
+
+print(get_indices_of_item_weights(weights, length, limit))  # -> (3, 1)
